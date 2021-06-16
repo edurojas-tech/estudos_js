944a7910ca16e74cc33a12f4ff937b2e3725e19f
Eduardo Rojas <edurojas.developer@gmail.com>
2021-06-15 23:31:48 -0300

    update

--- js/poo.js
+++ js/poo.js
@@ -1,15 +1,51 @@
+// Sem função construtora....
 function Book(title, pages, author) {
   (this.title = title), (this.pages = pages), (this.author = author);
 }
 
+function Food(name, type, quantity) {
+  (this.name = name), (this.type = type), (this.quantity = quantity);
+}
+
+// ============ arrays objetos ===============
 const books = [
   new Book("Mais esperto que o Diabo", 250, "Napoleón Hill"),
   new Book("Administração e Planejamento", 198, "Paulo Sertek"),
   new Book("Empreendedorismo", 123, "Paulo Sertek"),
 ];
 
-console.log(
-  books.filter((x) => {
-    return x.author == "Paulo Sertek";
-  })
-);
+const foods = [
+  new Food("Pizza Espanhola", "Massa Italiana", 10),
+  new Food("Frango a Passarinho", "Comida Brasileira", 7),
+];
+
+// ============ modo literal ============
+const Home = {
+  dono: "Edu",
+  tipo: "Apartamento",
+  casa: [],
+  showCasa() {
+    this.casa.push(this.dono, this.tipo);
+    console.log(this.casa);
+  },
+};
+Home.showCasa();
+
+// ============ função construtora ============
+class Product {
+  constructor(name, tipo, marca) {
+    (this.name = name), (this.tipo = tipo);
+    this.marca = marca;
+  }
+
+  showDetails() {
+    console.log(this);
+  }
+}
+
+const produto1 = new Product("Computador VivoBook", "Tech", "Asus");
+const produto2 = new Product("Smartphone Redmi 9", "Tech", "Xiaomi");
+produto1.showDetails();
+produto2.showDetails();
+
+// end code!
