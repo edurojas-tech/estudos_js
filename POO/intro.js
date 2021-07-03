7a7694f7d569c45d9700cc4f39e3bb53795b6724
Eduardo Rojas <edurojas.developer@gmail.com>
2021-07-03 01:32:55 -0300

    update poo

--- POO/intro.js
+++ POO/intro.js
@@ -1,7 +1,53 @@
 // Paradigma de Orientação a Objetos ou Programação Orientado a Objetos
 
-/*
+//Procedural
+let x = 5; let y = 3; let op = 'mult'
+function calcular(v1, v2, op){
+    switch(op){
+        case 'somar':
+            console.log(v1 + v2)
+        break;
 
+        case 'sub':
+            console.log(v1 - v2)
+        break;
+
+        case 'mult':
+            console.log(v1 * v2)
+        break;
+
+        case 'div':
+            console.log(v1 / v2)
+        break;
+    }
+}
+
+calcular(x, y, op)
+
+//POO
+const calcularr = {
+    v1: 100,
+    v2: 30,
+    op: "somar",
+    operacao(){
+        switch(this.op){
+            case 'somar':
+                console.log(this.v1 + this.v2)
+            break;
     
+            case 'sub':
+                console.log(this.v1 - this.v2)
+            break;
+    
+            case 'mult':
+                console.log(this.v1 * this.v2)
+            break;
+    
+            case 'div':
+                console.log(this.v1 / this.v2)
+            break;
+        }
+    }
+}
 
-*/
+calcularr.operacao()
