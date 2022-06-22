0f6dd1ecce391080f56d0b1a8cd1468847075106
Eduardo Rojas <edurojas.developer@gmail.com>
2022-06-22 01:14:53 -0300

    funcao com parametro default

---
+++ javascript-essencial/funcao_parametro_default.js
@@ -0,0 +1,13 @@
+function Cadastrar(nome = 'Eduardo', curso = 'POO com JS', idade = 29){
+  console.log(`
+      ==== Ficha Cadastro ===
+      Nome: ${nome},
+      Curso ${curso},
+      Idade: ${idade}
+      ======================
+  `)
+}
+
+Cadastrar('Gabrielle', 'Contabilidade', 31)
+Cadastrar('Eduardo Rojas', 'CST Coding', 22)
+Cadastrar('Betty Carolina', 'Design', 15)
