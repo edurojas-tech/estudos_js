445e3501be08a59efd7d1b57d640c083ce298914
Eduardo Dantas <eduardo.dantas@ascential.com>
2022-08-31 22:38:44 -0300

    up

--- javascript-essencial/variaveis.js
+++ javascript-essencial/variaveis.js
@@ -1,31 +1,26 @@
-const variaveis = {
-    nome : "Eduardo Rojas",  //tipo string
-    idade : 25,              //tipo number inteiro
-    altura : 1.72,           //tipo number float
-    temMoto : true,          //tipo boolean verdadeiro
-    temCarro : false,        //tipo boolean falso
-    estadoCivil : null,      //tipo Null ou Nulo
-    valorIndefinido,        //tipo undefined
-    passatempo : ["treinar musculação", "Leitura","Assistir Marvel"],   //tipo array
-
-    //executando no console do node.js
-    showVars(){
-        console.log(this.nome);
-        console.log(this.idade);
-        console.log(this.altura);
-        console.log(this.temMoto);
-        console.log(this.temCarro);
-        console.log(this.estadoCivil);
-        console.log(this.valorIndefinido);
+// === Variáveis e seus tipos ===
+var tipoString = 'instruções de caractéres ou textos'   //type string
+var tipoNumero = 22                                     //type number int
+var tipoNumeroFloat = 55.15                             //type number float
+var tipoNulo = null                                     // nulo
+var tipoArray = ['1', '2', '3']                         // array - Object
+var tipoObjeto = {                                      // Object
+    cor: "preto com laranja",
+    tipo: "estojo",
+    aberto: true,
+    status() {
+        let tipoBoolean = true
+        this.aberto && console.log('está aberto!')
+        console.log(`Dentro do bloco status: ${tipoBoolean}`) // -> dentro do escopo local
     }
-
-    /*
-        ::: Glossário do JS :::
-        O código 'console.log()', recebe as variáveis para ser exibida no console do node, 
-        o mesmo pode ser exibido no console do browser.
-        Este código é do tipo método do Objeto console, ou uma função nativa da linguagem.
-
-        variáveis dentro de um objeto, são propriedades ou valores, já funções são métodos.
-    */
 }
-variaveis.showVars();
+
+                        
+
+
+let tipoBoolean = 'agora mudou' // escopo global
+tipoObjeto.status()
+console.log(typeof(tipoArray))
+
+let tipoObjeto2 = 54545
+console.log(tipoObjeto2)
