3708595f89500dd621e50ce760ff8a8b2fa6ed1a
Eduardo Dantas <eduardo.dantas@ascential.com>
2022-09-20 00:28:32 -0300

    novo exemplo pratico

---
+++ exemplos-praticos/05-galeria/README.md
@@ -0,0 +1,9 @@
+# Conceitos aplicados
+
+variaveis
+comentarios
+estrurura condição (if-else, ternario)
+estrutura de repetição (for)
+funções e arrow function
+
+
---
+++ exemplos-praticos/05-galeria/image.js
@@ -0,0 +1,22 @@
+// lógica para galeria
+const galeria = document.getElementById('galeria')
+const tagImages = document.querySelectorAll(".img-thumbnail")
+const urlImages = [
+  "pWkk7iiCoDM",
+  "aob0ukAYfuI",
+  "EUfxH-pze7s",
+  "2gYsZUmockw",
+  "M185_qYH8vg",
+  "sesveuG_rNo",
+  "p2TQ-3Bh3Oo",
+  "EMSDtjVHdQ8",
+  "G9Rfc1qccH4"
+]
+
+function urlImagem (parametroLink) {
+  return `https://source.unsplash.com/${parametroLink}/400x300`
+}
+
+for(i = 0; i <= tagImages.length; i++){
+  tagImages[i].src = urlImagem(urlImages[i])
+}
