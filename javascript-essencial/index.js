8de2324453e9bd9c500d6df10c52b409ed306eaf
Eduardo Dantas <eduardo.dantas@ascential.com>
2022-09-21 13:52:16 -0300

    atualização e refatoração dos estudos js

--- javascript-essencial/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app = express()
-const port = 5000
-
-app.listen(port, error =>{
-    if(error){console.log('aconteceu um error ' + error)}
-    else{console.log('deu bom!')}
-})
