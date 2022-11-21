82f1ecd4ea78d543499cfd02adc6c2fc5a0080e0
Eduardo Dantas <eduardo.dantas@ascential.com>
2022-11-20 21:55:53 -0300

    criação da estrutura base do projeto

--- exemplos-praticos/06-consumo-api/fetch03/js/app.js
+++ exemplos-praticos/06-consumo-api/fetch03/js/app.js
@@ -1,5 +1,5 @@
-const urlAPI = "https://pokeapi.co/api/v2/pokemon?limit=42&offset=0"
+const urlAPI = 
+  "https://pokeapi.co/api/v2/pokemon?limit=42&offset=0"
 
 fetch(urlAPI).then((response) =>
-  response.json().then((resp) => inner('app', resp.results))
-)
+  response.json().then((resp) => inner('app', resp.results)))
