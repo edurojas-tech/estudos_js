edbef81822cb50169234c7c451def3cdda306b1f
Eduardo Dantas <eduardo.dantas@ascential.com>
2022-11-27 20:02:47 -0300

    feat(udemy): reorganizando as estrutura de pastas sobre estudos do JS

--- javascript-essencial/09-template-string/index.js
+++ javascript-essencial/09-template-string/index.js
@@ -1,9 +1,7 @@
 /*
-  template string, é um jeito mais simples de você concatenar variáveis com textos
-  ou conjunto de caractéres
-
-  Exemplos:
+  template string, é um jeito mais simples de você concatenar variáveis 
+  com textos ou conjunto de caractéres
 */
 
 let msg = text => `Descrição da mensagem: ${text}`
-console.log(msg('Nota da prova'))
+console.log(msg('Sua fatura vence dia 28/12/2021'))
